17ecddeb2adcfbfd1f309f77b3cd0389ac241bb6
wilfnerd <wilfredxmmanuel77@gmail.com>
2025-09-02 15:55:12 +0100

    Blog page fixed

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+// src/app/blog/page.tsx
+
+// A function that returns a JSX element
+export default function BlogPage() {
+  return (
+    <div>
+      <h1>Blog Posts</h1>
+      <p>This is my blog page.</p>
+    </div>
+  );
+}
